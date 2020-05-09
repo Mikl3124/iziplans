5a021f3b872ffd7ee4d366e1e21e88367225e07f
Mickael Delpech <mickael.delpech@gmail.com>
2020-05-09 12:14:06 +0200

    changed flashy v_6

--- resources/views/projets/show.blade.php
+++ resources/views/projets/show.blade.php
@@ -296,10 +296,7 @@
                                 <div class="col-md-2 col-sm-6">
                                     @auth
                                         @if (Auth::user()->avatar === NULL)
-                                            <div class="item">
-                                                <span class="notify-badge">NEW</span>
-                                                <img id="blah" class="mr-3 rounded card-avatar" src="https://iziplans.s3.eu-west-3.amazonaws.com/images/avatar.png">
-                                            </div>
+                                            <img id="blah" class="mr-3 rounded card-avatar" src="https://iziplans.s3.eu-west-3.amazonaws.com/images/avatar.png">
                                         @else
                                             <img id="blah" class="mr-3 rounded card-avatar" src="{{ Storage::url('/users/medium/'. $offer->user->avatar) }}">
                                         @endif
