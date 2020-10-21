9312d595acd7e81919edf06eadc69603e89da9bf
Mickael Delpech <mickael.delpech@gmail.com>
2020-10-21 12:49:59 +0200

    fix mapbox bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,9 +35,6 @@
     <script src="{{ asset('js/app.js') }}" ></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/js/select2.min.js"></script>
-    <!-- Mapbox  -->
-    <script src='https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js'></script>
-    <link href='https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css' rel='stylesheet' />
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -63,6 +60,10 @@
 
       gtag('config', 'UA-146702848-2');
     </script>
+
+    <!-- Mapbox  -->
+    <script src='https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css' rel='stylesheet' />
 
 
 </head>
