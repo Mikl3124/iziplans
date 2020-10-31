de4829937ce9bebe33210c1bb2aa8b9356ed3247
Mickael Delpech <mickael.delpech@gmail.com>
2020-10-31 20:47:57 +0100

    changed show bog article

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -5,18 +5,12 @@
     <div class="container">
 
       <div class="row">
-
         <!-- Post Content Column -->
-        <div class="col-lg-12">
-
-          <!-- Title -->
+        <div class="col-md-9 col-sm-12">
           <h1 class="mt-4">{{ $article->title }}</h1>
-
-          <!-- Author -->
-          <i class="lead">
+          <i>
             par {{ $article->user->firstname }} le {{ Carbon\Carbon::parse($article->created_at)->isoFormat('LL') }}
-          </>
-
+          </i>
           <hr>
 
           <!-- Preview Image -->
@@ -30,6 +24,20 @@
           <hr>
 
         </div>
+        <div class="col-md-3 col-sm-12">
+          <div class="card mt-4">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div class="text-center">
+                <a class="btn btn-success " href="http://">Besoin de plans?</a>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
 
     </div>
     <!-- /.container -->
