cd6e2bd43163dbff8649b16f03912c24c00408c9
Mickael Delpech <mickael.delpech@gmail.com>
2021-03-10 20:55:11 +0100

    debug category

--- app/Model/Projet.php
+++ app/Model/Projet.php
@@ -5,10 +5,8 @@ namespace App\Model;
 use App\Model\User;
 use App\Model\Offer;
 use App\Model\Budget;
-use App\Model\Projet;
 use App\Model\Message;
 use App\model\Category;
-use App\model\Competence;
 use App\Model\Departement;
 use Illuminate\Database\Eloquent\Model;
 use CyrildeWit\EloquentViewable\InteractsWithViews;
@@ -16,37 +14,36 @@ use CyrildeWit\EloquentViewable\Contracts\Viewable;
 
 class Projet extends Model implements Viewable
 {
-        protected $guarded =[];
-        use InteractsWithViews;
+  protected $guarded = [];
+  use InteractsWithViews;
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 
-    public function categories()
-    {
-        return $this->belongsToMany(Category::class);
-    }
+  public function categories()
+  {
+    return $this->belongsToMany(Category::class);
+  }
 
-    public function departement()
-    {
-        return $this->belongsTo(Departement::class);
-    }
+  public function departement()
+  {
+    return $this->belongsTo(Departement::class);
+  }
 
-    public function budget()
-    {
-        return $this->belongsTo(Budget::class);
-    }
+  public function budget()
+  {
+    return $this->belongsTo(Budget::class);
+  }
 
-    public function offers()
-    {
-        return $this->hasMany(Offer::class);
-    }
+  public function offers()
+  {
+    return $this->hasMany(Offer::class);
+  }
 
-    public function messages()
-    {
-        return $this->belongsToMany(Message::class);
-    }
-
+  public function messages()
+  {
+    return $this->belongsToMany(Message::class);
+  }
 }
