4481c5138ee6c31200926c16c5a484b5137ff6da
Mickael Delpech <mickael.delpech@gmail.com>
2021-03-18 13:29:58 +0100

    add filename to articles table 5

--- database/migrations/2020_02_20_223259_create_threads_table.php
+++ database/migrations/2020_02_20_223259_create_threads_table.php
@@ -34,8 +34,6 @@ class CreateThreadsTable extends Migration
    */
   public function down()
   {
-    DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-    Schema::dropIfExists('topics');
-    DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    Schema::dropIfExists('budgets');
   }
 }
