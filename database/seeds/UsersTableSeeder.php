3747e394f6e728aa93c172618b6121b0c86fedfe
Mickael Delpech <mickael.delpech@gmail.com>
2021-04-10 21:17:41 +0200

    added mail admin

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,9 +13,9 @@ class UsersTableSeeder extends Seeder
   // {
   //  DB::table('users')->insert([
   //      [
-  //         'firstname' => 'Mickael',
-  //         'lastname'  => 'Delpech',
-  //         'email' => 'mickael.delpech@gmail.com',
+  //         'firstname' => 
+  //         'lastname'  => 
+  //         'email' => 
   //         'password' => bcrypt('password'),
   //         'email_verified_at' => '2020-01-17 10:21:11',
   //         'role' =>'admin',
