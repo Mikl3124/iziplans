e5abe46ad504de69fb4626328da1ea6c9a1f7207
Mickael Delpech <mickael.delpech@gmail.com>
2021-04-22 14:25:18 +0200

    added sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($articles as $article)
         <url>
-            <loc>{{url($article->url)}}</loc>
+            <loc>blog/{{url($article->url)}}</loc>
             <lastmod>{{ gmdate('Y-m-d\TH:i:s\Z',strtotime($article->updated_at)) }}</lastmod>
             <changefreq>daily</changefreq>
             <priority>0.6</priority>
