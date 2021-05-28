31232d745e15a215a19984264e9a29ce1d4f378b
Mickael Delpech <mickael.delpech@gmail.com>
2021-05-28 13:30:20 +0200

    added maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<title>Site en maintenance</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>Nous revenons bientôt</h1>
+    <div>
+        <p>Désolé, le site est en maintenance. Vous pouvez toujours <a href="mailto:mickael@iziplans.com">nous contacter</a>, mais ne vous inquiétez pas, nous revenons très vite!</p>
+
+    </div>
+</article>
