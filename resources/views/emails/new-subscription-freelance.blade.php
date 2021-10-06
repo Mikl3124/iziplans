1d00dbea5f9d7680637c5ad910139db93d751570
Mickael Delpech <mickael.delpech@gmail.com>
2021-10-06 22:15:34 +0200

    style welcome email

--- resources/views/emails/new-subscription-freelance.blade.php
+++ resources/views/emails/new-subscription-freelance.blade.php
@@ -35,22 +35,13 @@
 
 	<tr>
 		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0px; margin: 0px; padding: 5px 6.25% 3px; width: 87.5%; font-size: 18px; font-weight: 300; line-height: 150%; color: rgb(0, 0, 0); font-family: sans-serif;">
-				Merci pour ton inscription et bienvenue !
-		</td>
-	</tr>
-	<tr>
-		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0px; margin: 0px; padding: 5px 6.25% 3px; width: 87.5%; font-size: 18px; font-weight: 300; line-height: 150%; color: rgb(0, 0, 0); font-family: sans-serif;">
-				Pour pouvoir accéder et profiter pleinement de l’expérience Yalink il te suffit de
+				Merci pour ton inscription {{ $user->firstname }} et bienvenue !
 		</td>
 	</tr>
 
-
-
-
-
 	<tr>
-		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0px; margin: 0px; padding: 25px 6.25% 5px; width: 87.5%;"><a href="https://app.yalink.fr/freelance/register?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MzM3MDU1MDgsImlhdCI6MTYzMzUzMjcwOCwic3ViIjo0NjUsImFjdGlvbiI6ImVtYWlsX3ZlcmlmaWNhdGlvbiJ9.8uktk172nOjse5uOXMaxCqeYnvDr85TSLMIrHNTj7_o" style="text-decoration: underline;" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://app.yalink.fr/freelance/register?token%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MzM3MDU1MDgsImlhdCI6MTYzMzUzMjcwOCwic3ViIjo0NjUsImFjdGlvbiI6ImVtYWlsX3ZlcmlmaWNhdGlvbiJ9.8uktk172nOjse5uOXMaxCqeYnvDr85TSLMIrHNTj7_o&amp;source=gmail&amp;ust=1633635461935000&amp;usg=AFQjCNHg2LPolmd6wzsXId7xiOObPEsEZQ" data-mt-detrack-inspected="true">
-				<table border="0" cellpadding="0" cellspacing="0" align="center" style="max-width: 240px; min-width: 120px; border-collapse: collapse; border-spacing: 0px; padding: 0px;"><tbody><tr><td align="center" valign="middle" style="padding: 12px 24px; margin: 0px; text-decoration: underline; border-collapse: collapse; border-spacing: 0px; border-radius: 4px;" bgcolor="#F8C708"><a style="text-decoration: underline; color: rgb(255, 255, 255); font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 120%;" href="https://app.yalink.fr/freelance/register?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MzM3MDU1MDgsImlhdCI6MTYzMzUzMjcwOCwic3ViIjo0NjUsImFjdGlvbiI6ImVtYWlsX3ZlcmlmaWNhdGlvbiJ9.8uktk172nOjse5uOXMaxCqeYnvDr85TSLMIrHNTj7_o" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://app.yalink.fr/freelance/register?token%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MzM3MDU1MDgsImlhdCI6MTYzMzUzMjcwOCwic3ViIjo0NjUsImFjdGlvbiI6ImVtYWlsX3ZlcmlmaWNhdGlvbiJ9.8uktk172nOjse5uOXMaxCqeYnvDr85TSLMIrHNTj7_o&amp;source=gmail&amp;ust=1633635461935000&amp;usg=AFQjCNHg2LPolmd6wzsXId7xiOObPEsEZQ" data-mt-detrack-inspected="true">
+		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0px; margin: 0px; padding: 25px 6.25% 5px; width: 87.5%;"><a href="https://iziplans.com" style="text-decoration: underline;" target="_blank">
+				<table border="0" cellpadding="0" cellspacing="0" align="center" style="max-width: 240px; min-width: 120px; border-collapse: collapse; border-spacing: 0px; padding: 0px;"><tbody><tr><td align="center" valign="middle" style="padding: 12px 24px; margin: 0px; text-decoration: underline; border-collapse: collapse; border-spacing: 0px; border-radius: 4px;" bgcolor="#0072FF"><a style="text-decoration: underline; color: rgb(255, 255, 255); font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 120%;" href="https://iziplans.com" target="_blank">
 						Aller sur iziplans
 					</a>
 			</td></tr></tbody></table></a>
@@ -58,10 +49,9 @@
 	</tr>
 
 
-
 	<tr>
 		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0px; margin: 0px; padding: 25px 6.25% 0px; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%; color: rgb(0, 0, 0); font-family: sans-serif;">
-				<b>Pense à compléter ton profil Yalink pour recevoir plus de missions.</b>
+				<b>Pense à compléter ton profil sur pour recevoir plus de missions.</b>
 		</td>
 	</tr>
 	<tr>
@@ -81,36 +71,11 @@
 
 	<tr>
 		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0px; margin: 0px; padding: 20px 6.25% 25px; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%; color: rgb(0, 0, 0); font-family: sans-serif;">
-				Une&nbsp;question? <a href="mailto:support@yalink.fr" style="color: rgb(18, 125, 179); font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 160%;" target="_blank" data-mt-detrack-inspected="true">support@yalink.fr</a>
+				Une&nbsp;question? <a href="mailto:mickael@iziplans.com" style="color: rgb(18, 125, 179); font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 160%;" target="_blank" data-mt-detrack-inspected="true">mickael@iziplans.com</a>
 		</td>
 	</tr>
 
 
 </tbody></table>
-
-
-
-<table border="0" cellpadding="0" cellspacing="0" align="center" width="560" style="border-collapse: collapse; border-spacing: 0px; padding: 0px; width: inherit; max-width: 560px;">
-
-
-
-	<tbody><tr>
-		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0px; margin: 0px; padding: 25px 6.25% 0px; width: 87.5%;"><table width="256" border="0" cellpadding="0" cellspacing="0" align="center" style="border-collapse: collapse; border-spacing: 0px; padding: 0px;">
-			<tbody><tr>
-
-
-				<td align="center" valign="middle" style="margin: 0px; padding: 0px 10px; border-collapse: collapse; border-spacing: 0px;"><a href="https://www.linkedin.com/company/yalinkommunity/" style="text-decoration: none;" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.linkedin.com/company/yalinkommunity/&amp;source=gmail&amp;ust=1633635461935000&amp;usg=AFQjCNGw6AyO9Cm8U7Cos60-BCw3cKI9xw" data-mt-detrack-inspected="true"><img border="0" vspace="0" hspace="0" style="padding: 0px; margin: 0px; outline: none; text-decoration: none; border: none; display: inline-block; color: rgb(0, 0, 0);" alt="F" title="Linkedin" width="0" height="0" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"></a></td>
-
-			</tr>
-			</tbody></table>
-		</td>
-	</tr>
-
-
-
-</tbody></table>
-
-
-</td></tr></tbody></table><div class="yj6qo"></div><div class="adL">
 
 </div></div>
