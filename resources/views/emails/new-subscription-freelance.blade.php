66ed0ad2f8ecc720a9698ff6f70cc6698d44ea18
Mickael Delpech <mickael.delpech@gmail.com>
2021-10-07 16:19:59 +0200

    style welcome email

--- resources/views/emails/new-subscription-freelance.blade.php
+++ resources/views/emails/new-subscription-freelance.blade.php
@@ -13,13 +13,13 @@
 
   <table border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF" width="560" style="border-collapse: collapse; border-spacing: 0px; padding: 0px; width: inherit; max-width: 560px;">
     <tbody>
-    <tr>
-      <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0px; padding: 25px 6.25% 0px; width: 87.5%; font-size: 24px; font-weight: bold; line-height: 130%; color: rgb(0, 0, 0); font-family: sans-serif;">
+      <tr>
+      <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0px; padding-bottom: 50px; padding: 25px 6.25% 0px; width: 87.5%; font-size: 24px; font-weight: bold; line-height: 130%; color: rgb(0, 0, 0); font-family: sans-serif;">
           Bienvenue sur iziplans, {{ $user->firstname }}! 👋
       </td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="border-collapse: collapse; border-spacing: 0px; margin-top: 50px; padding: 5px 6.25% 3px; width: 87.5%; font-size: 18px; font-weight: 300; line-height: 150%; color: rgb(0, 0, 0); font-family: sans-serif;">
+      <td align="left" valign="top" style="border-collapse: collapse; border-spacing: 0px; margin: 0px; padding: 5px 6.25% 3px; width: 87.5%; font-size: 18px; font-weight: 300; line-height: 150%; color: rgb(0, 0, 0); font-family: sans-serif;">
         Moi c'est Mickael, le fondateur d'iziplans.
       </td>
     </tr>
@@ -43,18 +43,15 @@
         A bientôt, sur <a href="https://iziplans.com">iziplans</a>
       </td>
     </tr>
-    <tr style="text-align:center">
-      <td width="100%">
-          <img style="height:50px" src="https://iziplans.s3.eu-west-3.amazonaws.com/images/iziplans-logo.png" alt="logo iziplans">
-      </td>
-    </tr>
     <tr>
       <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0px; margin: 0px; padding: 25px 6.25% 0px; width: 87.5%;"><hr color="#E0E0E0" align="center" width="100%" size="1" noshade="" style="margin: 0px; padding: 0px;">
       </td>
     </tr>
-
-
-
+    <tr style="text-align:center">
+      <td width="100%">
+          <img style="height:50px; margin-top:30px" src="https://iziplans.s3.eu-west-3.amazonaws.com/images/iziplans-logo.png" alt="logo iziplans">
+      </td>
+    </tr>
     <tr>
       <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0px; margin: 0px; padding: 20px 6.25% 25px; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%; color: rgb(0, 0, 0); font-family: sans-serif;">
           Une&nbsp;question? <a href="mailto:mickael@iziplans.com" style="color: rgb(18, 125, 179); font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 160%;" target="_blank" data-mt-detrack-inspected="true">mickael@iziplans.com</a>
